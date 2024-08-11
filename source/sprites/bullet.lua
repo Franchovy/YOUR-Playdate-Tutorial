17d0edb6eb369fd732982fe617cff16f73b90afe
Maxime <maxrofra@gmail.com>
2024-08-11 14:08:11 +0200

    Added bullet destroy timer

--- source/sprites/bullet.lua
+++ source/sprites/bullet.lua
@@ -13,11 +13,17 @@ function Bullet:init()
     self.velocityY = 0
 end
 
+function Bullet:destroy()
+    self:remove()
+end
+
 function Bullet:spawn(posX, posY, velX, velY)
     self:add()
     self:moveTo(posX, posY)
     self.velocityX = velX
     self.velocityY = velY
+
+    self.destroyTimer = playdate.timer.new(1000, self.destroy, self)
 end
 
 function Bullet:update()
